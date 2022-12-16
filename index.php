80266109a4f6fd6d38f36ed36931d95a25220286
noguten <120639572+noguten@users.noreply.github.com>
2022-12-16 14:42:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,74 @@
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+
+<p>掲示板</p>
+
+<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>"> 
+<input type="text" name="personal_name"><br><br>
+<textarea name="contents" rows="8" cols="40">
+</textarea><br><br>
+<input type="submit" name="btn1" value="投稿する">
+</form>
+
 <?php
 
-print("Hello World");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    writeData();
+}
+
+readData();
+
+function readData(){
+    $keijban_file = 'keijiban.txt';
+
+    $fp = fopen($keijban_file, 'rb');
+
+    if ($fp){
+        if (flock($fp, LOCK_SH)){
+            while (!feof($fp)) {
+                $buffer = fgets($fp);
+                print($buffer);
+            }
+
+            flock($fp, LOCK_UN);
+        }else{
+            print('ファイルロックに失敗しました');
+        }
+    }
+
+    fclose($fp);
+}
+
+function writeData(){
+    $personal_name = $_POST['personal_name'];
+    $contents = $_POST['contents'];
+    $contents = nl2br($contents); #nl2br : 改行文字の前にHTMLの改行タグである<br>を挿入する関数 
+
+    $data = "<hr>\r\n";
+    $data = $data."<p>投稿者:".$personal_name."</p>\r\n";
+    $data = $data."<p>内容:</p>\r\n";
+    $data = $data."<p>".$contents."</p>\r\n";
+
+    $keijban_file = 'keijiban.txt'; #投稿された内容を保存するファイル名を「keijiban.txt」とする
+
+    $fp = fopen($keijban_file, 'ab'); #フォームから投稿があった時にファイルに追加で格納していくためモード「ab」でファイルを開く(バイナリモード)
+
+    if ($fp){
+        if (flock($fp, LOCK_EX)){
+            if (fwrite($fp,  $data) === FALSE){
+                print('ファイル書き込みに失敗しました');
+            }
+
+            flock($fp, LOCK_UN);
+        }else{
+            print('ファイルロックに失敗しました');
+        }
+    }
+
+    fclose($fp);
+}
+
+?>
+</body>
+</html>
